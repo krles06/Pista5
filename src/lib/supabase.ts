5ac25d93bb8bfd36cc13e4e12c4ba65b3c607a70
krles06 <121910210+krles06@users.noreply.github.com>
2026-04-20 15:34:53 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -14,5 +14,6 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
         storage: window.localStorage,
         autoRefreshToken: true,
         detectSessionInUrl: true,
+        flowType: 'pkce',
     }
 })
